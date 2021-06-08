5b9e6b34b32c66f118088b2cc0e690245f6250bb
Nick Aja <naja@hagerty.com>
2021-06-08 08:55:56 -0400

    createListItem fails with existing list item

--- src/routes/__tests__/list-items-controller.exercise.ts
+++ src/routes/__tests__/list-items-controller.exercise.ts
@@ -257,3 +257,38 @@ test('createListItem creates and returns a list item', async () => {
   expect(res.json).toHaveBeenCalledWith({listItem: {...createdListItem, book}})
   expect(res.json).toHaveBeenCalledTimes(1)
 })
+
+test('createListItem returns 400 error if user already has a list item for the given book', async () => {
+  const user = buildUser({id: 'FAKE_USER_ID'})
+  const book = buildBook({id: 'FAKE_BOOK_ID'})
+  const existingListItem = buildListItem({
+    bookId: book.id,
+    ownerId: user.id,
+  })
+  mockedListItemsDB.query.mockResolvedValueOnce([existingListItem])
+
+  const req = buildReq({
+    user,
+    body: {bookId: book.id},
+  })
+  const res = buildRes()
+
+  await listItemsController.createListItem(req, res)
+
+  expect(mockedListItemsDB.query).toHaveBeenCalledWith({
+    ownerId: user.id,
+    bookId: book.id,
+  })
+  expect(mockedListItemsDB.query).toHaveBeenCalledTimes(1)
+
+  expect(res.status).toHaveBeenCalledWith(400)
+  expect(res.status).toHaveBeenCalledTimes(1)
+  expect(res.json.mock.calls[0]).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "message": "User FAKE_USER_ID already has a list item for the book with the ID FAKE_BOOK_ID",
+      },
+    ]
+  `)
+  expect(res.json).toHaveBeenCalledTimes(1)
+})
