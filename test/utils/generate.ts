2975dee57e0880f3bf1b9f84766c0b2acc2da181
Nick Aja <naja@hagerty.com>
2021-06-08 09:08:33 -0400

    enhance generators

--- test/utils/generate.ts
+++ test/utils/generate.ts
@@ -6,42 +6,56 @@ import {getUserToken, getSaltAndHash} from '../../src/utils/auth'
 // prefex all of the ones we generate with `!0_Oo` to ensure it's valid.
 const getPassword = (...args) => `!0_Oo${faker.internet.password(...args)}`
 const getUsername = faker.internet.userName
-const getId = faker.random.uuid
+const getId = faker.datatype.uuid
 const getSynopsis = faker.lorem.paragraph
 const getNotes = faker.lorem.paragraph
 
 function buildUser({
+  id = getId(),
+  username = getUsername(),
   password = getPassword(),
   ...overrides
 }: Partial<User> = {}): User {
   return {
-    id: getId(),
-    username: getUsername(),
+    id,
+    username,
     ...getSaltAndHash(password),
     ...overrides,
   }
 }
 
-function buildBook(overrides: Partial<Book> = {}): Book {
+function buildBook({
+  id = getId(),
+  title = faker.lorem.words(),
+  author = faker.name.findName(),
+  coverImageUrl = faker.image.imageUrl(),
+  pageCount = faker.random.number(400),
+  publisher = faker.company.companyName(),
+  synopsis = faker.lorem.paragraph(),
+  ...overrides
+}: Partial<Book> = {}): Book {
   return {
-    id: getId(),
-    title: faker.lorem.words(),
-    author: faker.name.findName(),
-    coverImageUrl: faker.image.imageUrl(),
-    pageCount: faker.random.number(400),
-    publisher: faker.company.companyName(),
-    synopsis: faker.lorem.paragraph(),
+    id,
+    title,
+    author,
+    coverImageUrl,
+    pageCount,
+    publisher,
+    synopsis,
     ...overrides,
   }
 }
 
-function buildListItem(overrides: Partial<ListItem>): ListItem {
-  const {
-    bookId = overrides.bookId ?? getId(),
-    startDate = faker.date.past(2),
-    finishDate = faker.date.between(startDate, new Date()),
-    ownerId = overrides.ownerId ?? getId(),
-  } = overrides
+function buildListItem({
+  id = getId(),
+  bookId = getId(),
+  ownerId = getId(),
+  rating = faker.random.number(5),
+  notes = getNotes(),
+  startDate = faker.date.past(2),
+  finishDate = faker.date.between(startDate, new Date()),
+  ...overrides
+}: Partial<ListItem> = {}): ListItem {
   return {
     id: getId(),
     bookId,
